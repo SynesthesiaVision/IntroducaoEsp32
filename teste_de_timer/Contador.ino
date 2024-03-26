c086be7f32a73b2104b3d8bea1e12e3c3d1d753c
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-03-26 14:52:11 -0300

    Update Contador.ino

--- teste_de_timer/Contador.ino
+++ teste_de_timer/Contador.ino
@@ -1,54 +1,52 @@
-// Definições de pinos
-int buttonPin = 2; 
-int ledPin = 13;  
-int timerInterval = 1000; 
+#define LED 12
 
 
-bool counting = false;
-bool debounce = false;  
-long startTime = 0; 
+hw_timer_t * timer = NULL;
 
-void IRAM_ATTR buttonInterrupt() {
-  if (startTime >= 1000){
-    debounce = false;
-  }
 
-if(debounce == false){
-  if (digitalRead(buttonPin) == HIGH) {
-    if (!counting) {  // Se não está contando, inicia a contagem
-      startTime = millis(); // Armazena o tempo de início
-      counting = true;  // Define a flag de contagem como verdadeira
-      debounce = true;
-    } else {  // Se está contando, para a contagem
-      counting = false;  // Define a flag de contagem como falsa
-      startTime = 0;  // Reinicia o tempo de início
-      Serial.println("Contagem interrompida.");
-    }
-  }
-}
-}
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, RISING);
-}
-
-void loop() {
-  if (counting) {
-    unsigned long currentTime = millis();
-    unsigned long elapsedTime = currentTime - startTime;
-    Serial.print("Tempo decorrido: ");
-    Serial.print(elapsedTime / 1000);  // Exibe o tempo decorrido em segundos
+void cb_timer(){
+    unsigned int counter = 1;
+    Serial.print(millis()/1000);
     Serial.println(" segundos");
-    
-    // Acende o LED após 5 segundos
-    if (elapsedTime >= 5000) {
-      digitalWrite(ledPin, HIGH);
-    }
-  } else {
-    digitalWrite(ledPin, LOW);  // Garante que o LED esteja apagado quando a contagem estiver parada
-  }
-  delay(100);  // Pequena pausa para estabilidade
+   
+    counter++;
 }
+
+void startTimer(){
+    //inicialização do timer. Parametros:
+    /* 0 - seleção do timer a ser usado, de 0 a 3.
+      80 - prescaler. O clock principal do ESP32 é 80MHz. Dividimos por 80 para ter 1us por tick.
+    true - true para contador progressivo, false para regressivo
+    */
+    timer = timerBegin(0, 80, true);
+
+    /*conecta à interrupção do timer
+     - timer é a instância do hw_timer
+     - endereço da função a ser chamada pelo timer
+     - edge=true gera uma interrupção
+    */
+    timerAttachInterrupt(timer, &cb_timer, true);
+
+    /* - o timer instanciado no inicio
+       - o valor em us para 1s
+       - auto-reload. true para repetir o alarme
+    */
+    timerAlarmWrite(timer, 1000000, true); 
+
+    //ativa o alarme
+    timerAlarmEnable(timer);
+}
+
+void stopTimer(){
+    timerEnd(timer);
+    timer = NULL; 
+}
+
+void setup(){
+    Serial.begin(115200);
+    startTimer();
+}
+
+void loop(){}
+    
